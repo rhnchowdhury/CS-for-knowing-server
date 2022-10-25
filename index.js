cfc03e219ea60ee5156979799260eb6399700b21
Raihan <rhnchdri@gmail.com>
2022-10-26 01:45:39 +0600

    all data create

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const subList = require('./data/list.json')
+app.use(cors());
+
+const subList = require('./data/list.json');
+const subShow = require('./data/subject.json');
+
 
 app.get('/sub-list', (req, res) => {
     res.send(subList);
-})
+});
+
+app.get('/subList/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedSub = subShow.find(sub => sub.id === id);
+    res.send(selectedSub);
+});
+
+app.get('/subject/:id', (req, res) => {
+    const id2 = req.params.id;
+    if (id2 === '08') {
+        res.send(subShow)
+    }
+    else {
+        const subDetails = subShow.find(s => s.sub_id == id2);
+        res.send(subDetails);
+    }
+    // const subDetails = subShow.find(s => s.sub_id == id2);
+    // res.send(subDetails);
+
+});
+
+app.get('/subject', (req, res) => {
+    res.send(subShow);
+});
 
 app.get('/', (req, res) => {
     res.send("Creating server side");
