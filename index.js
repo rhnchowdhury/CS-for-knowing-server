6dcf7abb8853fd2341f801d10ee7795720354e01
Raihan <rhnchdri@gmail.com>
2022-10-25 19:05:43 +0600

    list data generate

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+const subList = require('./data/list.json')
+
+app.get('/sub-list', (req, res) => {
+    res.send(subList);
+})
 
 app.get('/', (req, res) => {
     res.send("Creating server side");
